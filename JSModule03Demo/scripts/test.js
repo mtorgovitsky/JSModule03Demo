b51bab75b97b59282e22d91e1d287162cce9235a
Michael Torgovitsky <m.torgovitsky@gmail.com>
2017-04-04 17:13:06 +0300

    Add project files.

---
+++ JSModule03Demo/scripts/test.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    //your code here
+});
+
+$(document).ready(function () {
+    //$(".green").text("gggggg");
+});
+
+$(document).ready(
+    function () {
+        $("h2").each(
+            function fff() {
+                this.style.color = "red";
+            }
+        );
+    }
+);
+
+$(document).ready(
+    function () {
+        $("h2").eq(2).css("color", "green");
+    });
+
+//will work only onLoadDocument 
+ $("h3").eq(1).css("color", "green");
